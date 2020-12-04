4aa8526552e1b005d778a9fb95db6b8856eb7309
Igor Oliveira <igor.oliveira@meteogroup.com>
2020-12-04 13:05:06 +0100

    2015 day1

---
+++ 2015/day1.py
@@ -0,0 +1,23 @@
+with open("day1.input") as f:
+    instructions = f.read()
+
+print("Part1", instructions.count("(") - instructions.count(")"))
+
+
+def santa():
+    floor = 0
+    for index, instruction in enumerate(instructions):
+        if instruction == "(":
+            move = 1
+        else:
+            move = -1
+
+        floor += move
+
+        if floor == -1:
+            return index + 1
+
+    return None
+
+
+print("Part2", santa())
