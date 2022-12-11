60c8cb9647df863d703387b569aa43a231f0722d
Igor Oliveira <igor@dexterenergy.nl>
2022-12-11 22:55:19 +0100

    2015 day 10 submission

---
+++ 2015/10/main.py
@@ -0,0 +1,16 @@
+from itertools import groupby
+
+
+def look_and_say(sequence):
+    groups = ["".join(group) for _, group in groupby(sequence)]
+    return "".join([f"{len(group)}{min(set(group))}" for group in groups])
+
+
+number = "1113122113"
+for _ in range(40):
+    number = look_and_say(number)
+print("part 1:", len(number))
+
+for _ in range(10):
+    number = look_and_say(number)
+print("part 2:", len(number))
