3afca90ce2a1b0e209c65cb50e6c6bbb539805f0
Igor Oliveira <igor@dexterenergy.nl>
2022-12-13 17:16:32 +0100

    day 12 submission

--- 2022/12/main.py
+++ 2022/12/main.py
@@ -19,32 +19,33 @@ def render_path(points, elevation_data):
         print()
 
 
+def make_graph(elevation_2d_array):
+    graph = nx.DiGraph(nx.grid_2d_graph(*elevation_2d_array.shape))
+    for node in graph.nodes():
+        graph.nodes[node]['elevation'] = elevation_2d_array[node]
+
+    for edge in graph.edges.data():
+        diff = graph.nodes.data()[edge[1]]['elevation'] - graph.nodes.data()[edge[0]]['elevation']
+        edge[2]['weight'] = diff if diff <= 1 else np.inf
+    return graph
+
+
+def find_shortest_path_length(graph, source, target):
+    try:
+        return len(nx.bellman_ford_path(graph, source, target, 'weight')) - 1
+    except nx.exception.NetworkXNoPath:
+        return np.nan
+
+
 elevation, start, end = get_elevation_array()
+grid = make_graph(elevation)
 
-grid = nx.DiGraph(nx.grid_2d_graph(*elevation.shape))
-for node in grid.nodes():
-    grid.nodes[node]['elevation'] = elevation[node]
-
-for edge in grid.edges.data():
-    diff = grid.nodes.data()[edge[1]]['elevation'] - grid.nodes.data()[edge[0]]['elevation']
-    edge[2]['weight'] = diff if diff <= 1 else np.inf
-
-# path_p1 = nx.bellman_ford_path(grid, start, end, 'weight')
-path_p1 = nx.dijkstra_path(grid, start, end, 'weight')
-render_path(path_p1, elevation)
-print('part 1:', len(path_p1) - 1)
+length = find_shortest_path_length(grid, start, end)
+print('part 1:', length)
 
 origins = [i for i in zip(np.where(elevation == ord('a'))[0], np.where(elevation == ord('a'))[1])]
-
-path_lengths_p2 = []
-for origin in origins:
-    try:
-        path = nx.bellman_ford_path(grid, origin, end, 'weight')
-        path_lengths_p2.append(len(path) - 1)
-    except nx.exception.NetworkXNoPath:
-        pass
-
-print('part 2:', min(path_lengths_p2))
+lengths = [find_shortest_path_length(grid, origin, end) for origin in origins]  # .remove(None)
+print('part 2:', int(np.nanmin(lengths)))
 
 # part 1: 440
 # part 2: 439
