b1685f64433a9a04cb7185d8ec6bed4fa5bcc60e
Igor Oliveira <igor@source.ag>
2022-12-22 11:41:03 +0000

    refactor solution

--- 2015/01/main.py
+++ 2015/01/main.py
@@ -1,23 +1,12 @@
-with open("input") as f:
-    instructions = f.read()
-
-print("Part1", instructions.count("(") - instructions.count(")"))
-
-
-def santa():
-    floor = 0
+def santa(floor = 0):
     for index, instruction in enumerate(instructions):
-        if instruction == "(":
-            move = 1
-        else:
-            move = -1
-
-        floor += move
-
+        floor += 1 if instruction == "(" else -1
         if floor == -1:
             return index + 1
 
     return None
 
 
-print("Part2", santa())
+instructions = open("input").read()
+print("part1 :", instructions.count("(") - instructions.count(")"))
+print("part2 :", santa())
