d8e9ee8da099fc86576dd213e9039f5c096e22fe
Igor Oliveira <igoroliveira@dexternergy.nl>
2023-12-02 16:51:08 +0100

    submissions for 2019 days 2,3

--- 2019/02/main.py
+++ 2019/02/main.py
@@ -1,56 +1,38 @@
-import itertools
-
-import numpy as np
-
-
-def intcode(list_of_codes):
-    for pointer, code in enumerate(list_of_codes):
-        if pointer % 4 == 0:
-            opcode = code
-            if opcode == 99:
-                return list_of_codes
-        elif pointer % 4 == 1:
-            parameter_1 = list_of_codes[code]
-        elif pointer % 4 == 2:
-            parameter_2 = list_of_codes[code]
-        elif (pointer % 4 == 3) and (opcode == 1):
-            list_of_codes[code] = parameter_1 + parameter_2
-        elif (pointer % 4 == 3) and (opcode == 2):
-            list_of_codes[code] = parameter_1 * parameter_2
-        elif (pointer % 4 == 3) and (opcode == 99):
-            return list_of_codes
-
-    return list_of_codes
+def intcode(memory):
+    pos = 0
+    while memory[pos] != 99:
+        code = memory[pos]
+        val_1 = memory[memory[pos + 1]]
+        val_2 = memory[memory[pos + 2]]
+        result_pos = memory[pos + 3]
+        if code == 1:
+            memory[result_pos] = val_1 + val_2
+        elif code == 2:
+            memory[result_pos] = val_1 * val_2
+        else:
+            raise ValueError(f"Invalid opcode: {code}")
+        pos += 4
+    return memory[0]
 
 
-def reset_memory():
-    with open("input") as f:
-        lines = f.readlines()
-        for line in lines:
-            list_of_codes = list(np.fromstring(line, dtype=int, sep=","))
-    return list_of_codes
+def part2(target, memory):
+    for noun in range(100):
+        for verb in range(100):
+            this_memory = memory.copy()
+            this_memory[1] = noun
+            this_memory[2] = verb
+            result = intcode(this_memory)
+            if result == target:
+                return 100 * noun + verb
 
 
-def restore_gravity(list_of_codes):
-    list_of_codes[1] = 12
-    list_of_codes[2] = 2
-    return list_of_codes
+memory_safe = list(map(int, open("input", "r").read().split(",")))
+this_memory = memory_safe.copy()
+this_memory[1] = 12
+this_memory[2] = 2
 
+print("Part 1:", intcode(this_memory))
+print("Part 2:", part2(19690720, memory_safe))
 
-# Part 1
-list_of_codes = reset_memory()
-print(f"Part 1 : {intcode(restore_gravity(list_of_codes))[0]}")
-
-
-# Part 2
-def find_noun_and_verb():
-    for noun, verb in list(itertools.permutations(range(100), 2)):
-        list_of_codes = reset_memory()
-        list_of_codes[1] = noun
-        list_of_codes[2] = verb
-        list_of_codes = intcode(list_of_codes)
-        if list_of_codes[0] == 19690720:
-            print(f"Part 2 : {100 * noun + verb}")
-
-
-find_noun_and_verb()
+# Part 1: 9706670
+# Part 2: 2552
